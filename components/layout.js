f5606ad0d9b2b81ca9b18e07ba57382e2204c0bc
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2022-03-21 09:20:18 +1000

    hambergur menu for mibile

--- components/layout.js
+++ components/layout.js
@@ -10,54 +10,129 @@ Router.onRouteChangeError = (url) => NProgress.done();
 
 const Layout = ({ children }) => {
   const nav = () => (
-    <ul className="nav nav-tabs bg-primary variant-dark">
-      <li className="nav-item">
-        <Link href="/">
-          <a className="nav-link text-light">Home</a>
-        </Link>
-      </li>
-      <li className="nav-item submitLinkNav">
-        <Link href="/user/link/create">
-          <a className="nav-link text-light btn btn-success">Submit a Link</a>
-        </Link>
-      </li>
-      {!isAuth() && (
-        <>
-          <li className="nav-item">
-            <Link href="/login">
-              <a className="nav-link text-light">Login</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/register">
-              <a className="nav-link text-light">Register</a>
-            </Link>
-          </li>
-        </>
-      )}
-      {process.browser && isAuth() && isAuth().role === "admin" && (
-        <li className="nav-item ms-auto">
-          <Link href="/admin">
-            <a className="nav-link text-light">{isAuth().name}</a>
-          </Link>
-        </li>
-      )}
-      {process.browser && isAuth() && isAuth().role === "subscriber" && (
-        <li className="nav-item ms-auto">
-          <Link href="/user">
-            <a className="nav-link text-light">{isAuth().name}</a>
-          </Link>
-        </li>
-      )}
-      {process.browser && isAuth() && (
+    <>
+      <ul className="nav nav-tabs bg-primary variant-dark non-mobile-nav">
         <li className="nav-item">
-          <a onClick={logout} className="nav-link text-light">
-            Logout
-          </a>
+          <Link href="/">
+            <a className="nav-link text-light">Home</a>
+          </Link>
         </li>
-      )}
-      <li className="nav-item spinner-space"></li>
-    </ul>
+        <li className="nav-item submitLinkNav">
+          <Link href="/user/link/create">
+            <a className="nav-link text-light btn btn-success">Submit a Link</a>
+          </Link>
+        </li>
+        {!isAuth() && (
+          <>
+            <li className="nav-item">
+              <Link href="/login">
+                <a className="nav-link text-light">Login</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/register">
+                <a className="nav-link text-light">Register</a>
+              </Link>
+            </li>
+          </>
+        )}
+        {process.browser && isAuth() && isAuth().role === "admin" && (
+          <li className="nav-item ms-auto">
+            <Link href="/admin">
+              <a className="nav-link text-light">{isAuth().name}</a>
+            </Link>
+          </li>
+        )}
+        {process.browser && isAuth() && isAuth().role === "subscriber" && (
+          <li className="nav-item ms-auto">
+            <Link href="/user">
+              <a className="nav-link text-light">{isAuth().name}</a>
+            </Link>
+          </li>
+        )}
+        {process.browser && isAuth() && (
+          <li className="nav-item">
+            <a onClick={logout} className="nav-link text-light">
+              Logout
+            </a>
+          </li>
+        )}
+        <li className="nav-item spinner-space"></li>
+      </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary mobile-nav">
+        <div className="container-fluid">
+          <h4 className="navbar-brand">IT Learning Resources</h4>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link href="/">
+                  <a className="nav-link text-light">Home</a>
+                </Link>
+              </li>
+              {!isAuth() && (
+                <>
+                  <li className="nav-item">
+                    <Link href="/login">
+                      <a className="nav-link text-light">Login</a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/register">
+                      <a className="nav-link text-light">Register</a>
+                    </Link>
+                  </li>
+                </>
+              )}
+              {process.browser && isAuth() && isAuth().role === "admin" && (
+                <li className="nav-item ms-auto">
+                  <Link href="/admin">
+                    <a className="nav-link text-light">{isAuth().name}</a>
+                  </Link>
+                </li>
+              )}
+              {process.browser && isAuth() && isAuth().role === "subscriber" && (
+                <li className="nav-item ms-auto">
+                  <Link href="/user">
+                    <a className="nav-link text-light">{isAuth().name}</a>
+                  </Link>
+                </li>
+              )}
+              {process.browser && isAuth() && (
+                <li className="nav-item">
+                  <a onClick={logout} className="nav-link text-light">
+                    Logout
+                  </a>
+                </li>
+              )}
+              <li
+                className="nav-item submitLinkNav"
+                style={{ borderRadius: "1em" }}
+              >
+                <Link href="/user/link/create">
+                  <a
+                    className="nav-link text-light btn btn-success"
+                    style={{ borderRadius: "1em", backgroundColor: "#02b488" }}
+                  >
+                    Submit a Link
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/src/collapse.js";
 import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
